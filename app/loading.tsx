26b5004301443057e83df939b1bbf22e92d40d90
chewmountain <mironov.develop@gmail.com>
2023-12-06 15:32:42 +0300

    add: page routes loader

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="h-full flex items-center justify-center">
+            <h3>Loading...</h3>
+        </div>
+    );
+};
+
+export default Loading;
