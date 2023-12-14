b04cb67a988a2bb3632c4a9ede0a2e9d51a21034
chewmountain <mironov.develop@gmail.com>
2023-12-14 12:23:17 +0300

    add: organization id page w/ form and boards

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,27 @@
-const OrganizationIdPage = () => {
-    return <div>Organization Page</div>;
+import { db } from "@/lib/db";
+
+import { Board } from "./board";
+import { Form } from "./form";
+
+/**
+ * Для использования асинхронных функций/методов
+ * внутри серверных компонентов, такие компоненты нужно
+ * заворачивать в async.
+ */
+
+const OrganizationIdPage = async () => {
+    const boards = await db.board.findMany();
+
+    return (
+        <div className="flex flex-col space-y-4">
+            <Form />
+            <div className="space-y-2">
+                {boards.map(({ id, title }) => (
+                    <Board id={id} title={title} key={id} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default OrganizationIdPage;
