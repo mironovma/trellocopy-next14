800e9939d97745d307ddf7e680d880aa02d4b7dc
chewmountain <mironov.develop@gmail.com>
2023-12-18 16:58:44 +0300

    add: images remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        /**
+         * Необходимо указать с какими хостами (в данном случае с photohosting)
+         * можно нашему приложению взаимодействовать.
+         */
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "img.clerk.com",
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
             },
         ],
     },
